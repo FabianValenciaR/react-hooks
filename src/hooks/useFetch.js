5dd4291660aa5578e1865a2601ee58988250b2bd
Fabian Valencia <fabianmvr@gmail.com>
2021-03-02 12:19:29 -0500

    React Hook | Use Ref

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,19 +1,31 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export const useFetch = (url) => {
-    const [state, setState] = useState({ data: null, loading: true, error: null })
+    const isMounted = useRef(true);
+
+    const [state, setState] = useState({ data: null, loading: true, error: null });
+
+    useEffect(() => {
+        return () => {
+            isMounted.current = false;
+        }
+    }, [])
 
     useEffect(() => {
         setState({ data: null, loading: true, error: null });
-        
+
         fetch(url)
             .then(res => res.json())
             .then(data => {
-                setState({
-                    loading: false,
-                    error: null,
-                    data: data
-                })
+                setTimeout(() => {
+                    if (isMounted.current) {
+                        setState({
+                            loading: false,
+                            error: null,
+                            data: data
+                        })
+                    }
+                }, 4000);
             });
 
         return () => {
